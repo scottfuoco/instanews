7c7f35c26dacc6974eb16d5c221f15eb2dbb65e6
Scott <scottfuoco@gmail.com>
2016-10-15 15:42:16 -0700

    added loading image

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,34 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer'),
     sass = require('gulp-sass'),
     cssnano = require('gulp-cssnano'),
+    babel = require('gulp-babel'),
+    sourcemaps = require("gulp-sourcemaps"),
+    concat = require("gulp-concat"),
+    notify = require("gulp-notify"),
     browserSync = require('browser-sync').create();
+    
+    
+var plumberErrorHandler = {
+  errorHandler: notify.onError({
+    title: 'gulp',
+    message: 'Error: <%= error.message %>'
+  })
+};
 
 
 gulp.task('js-build', function() {
-    gulp.src('./js/*.js') // select all the javascript files in the js folder off of the project root folder
-      .pipe(plumber())
-      .pipe(uglify()) // minify/compress the selected files
-      .pipe(rename({ extname: '.min.js' })) //  rename all minified files to have the extension .min.js instead of just .js
-      .pipe(gulp.dest('./build/js')) // save all minified files in ./build/js
+    gulp.src('js/*.js') // select all the javascript files in the js folder off of the project root folder
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(sourcemaps.init())
+      .pipe(babel())
+      .pipe(uglify())      
+      .pipe(rename({ extname: '.min.js' }))
+      .pipe(gulp.dest('./build/js')) 
 });
 
-gulp.task('eslint', function() {
+gulp.task('lint', function() {
     return gulp.src(['js/*.js','!node_modules/**'])
-        .pipe(plumber())
+        .pipe(plumber(plumberErrorHandler))
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
@@ -31,28 +45,29 @@ gulp.task('BrowserReload', function(done) {
 })
 
 gulp.task('sass-build', function() {
-   gulp.src('./sass/style.scss')
+   gulp.src('sass/style.scss')
+      .pipe(plumber(plumberErrorHandler))
       .pipe(sass())
       .pipe(autoprefixer({
          browsers: ['last 2 versions']
       }))
-      .pipe(gulp.dest('./build/css'))
+      .pipe(gulp.dest('build/css'))
       .pipe(cssnano())
       .pipe(rename('style.min.css'))
-      .pipe(gulp.dest('./build/css'));
+      .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('serve', function () {
+gulp.task('watch', function () {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 
-    gulp.watch("js/*.js", ['js-build', 'eslint']);
+    gulp.watch("js/*.js", ['js-build', 'lint']);
     gulp.watch("sass/*.scss", ['sass-build']);
     
     gulp.watch(["build/css/*", "build/js/*", "index.html"], ["BrowserReload"]);
 });
 
-gulp.task('default', ['serve', 'eslint']);
+gulp.task('default', ['watch', 'lint']);
--- js/main.js
+++ js/main.js
@@ -1,40 +1,65 @@
 $(function() {
-  $('#section-select').on('change', function(e) {
-    e.preventDefault();
-    
-    var $newsStoriesList = $('#new-stories-list');    
+  
+var firstTimeSelecting = true;
+ 
+  $('#section-select').on('change', function() {
+    if (firstTimeSelecting) {      
+      var el = $('header'),
+      curHeight = el.height(),
+      autoHeight = el.css('height', 'auto').height();
+      
+      el.height(curHeight);
+      el.height(autoHeight);
+      
+      firstTimeSelecting = false;
+    }
+    var $newsStoriesList = $('#news-stories-list');    
     var selection = $('#section-select').val();
-    
-var url = "https://api.nytimes.com/svc/topstories/v2/"+selection+".json";
-url += '?' + $.param({
-  'api-key': "193e3d7d6bf949bc9e5d62ae2565e5eb"
-});
-   
+
+    var url = 'https://api.nytimes.com/svc/topstories/v2/'+selection+'.json';
+    url += '?' + $.param({
+      'api-key': '193e3d7d6bf949bc9e5d62ae2565e5eb'
+    });
+   $('.loading-image').show();
     $.ajax({
       url: url,
       method: 'GET',
     })
     .done(function(data) {        
         var newsStoriesMarkup = '';
-        var storiesCount = 0;
         
-        $.each(data.results, function( key, value ){
-          if (value.multimedia.length > 0){
-            newsStoriesMarkup +=   '<li><img src="'+ value.multimedia[3].url + '" />';
-            newsStoriesMarkup +=   '<p>'+value.abstract+'<p/></li>';
-            storiesCount++;
-            
-            if(storiesCount >= 11){
-              return false;
-            }
-          }
+        
+        
+        
+        var filteredData = data.results.filter(function(value){
+          return value.multimedia.length >= 5;
         });
+        
+        filteredData.splice(12);
+        
+        $.each(filteredData, function ( key, value ) {
+            newsStoriesMarkup += '<li>';
+            newsStoriesMarkup += '<a href="'+value.url+'">'
+            newsStoriesMarkup += '<div class="article-background" style="background-image:url('+ value.multimedia[4].url + ')">';
+            newsStoriesMarkup += '<p class="article-abstract">'+value.abstract;
+            newsStoriesMarkup += '</p></div></a></li>';
+        })
 
       $newsStoriesList.html(newsStoriesMarkup);
     })
     .fail(function(err) {
       throw err;
-    });
-    
+    })
+    .always(function(){              
+      $('.loading-image').hide();
+    })    
+  });
+  
+  $('#news-stories-list').on('mouseenter', 'li', function(){
+    $(this).find('.article-abstract').slideDown(1000);
+  });
+
+  $('#news-stories-list').on('mouseleave', 'li', function(){
+    $(this).find('.article-abstract').slideUp(1000);
   });
 });
