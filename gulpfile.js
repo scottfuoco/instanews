4679d17c830f2d0b41276a0fb177a42070701a40
Scott Fuoco <scottfuoco@gmail.com>
2016-10-17 00:31:26 -0700

    moving to laptop

--- gulpfile.js
+++ gulpfile.js
@@ -7,67 +7,66 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     cssnano = require('gulp-cssnano'),
     babel = require('gulp-babel'),
-    sourcemaps = require("gulp-sourcemaps"),
-    concat = require("gulp-concat"),
-    notify = require("gulp-notify"),
+    sourcemaps = require('gulp-sourcemaps'),
+    notify = require('gulp-notify'),
     browserSync = require('browser-sync').create();
-    
-    
+
+
 var plumberErrorHandler = {
-  errorHandler: notify.onError({
-    title: 'gulp',
-    message: 'Error: <%= error.message %>'
-  })
+    errorHandler: notify.onError({
+        title: 'gulp',
+        message: 'Error: <%= error.message %>'
+    })
 };
 
 
-gulp.task('js-build', function() {
-    gulp.src('js/*.js') // select all the javascript files in the js folder off of the project root folder
-      .pipe(plumber(plumberErrorHandler))
-      .pipe(sourcemaps.init())
-      .pipe(babel())
-      .pipe(uglify())      
-      .pipe(rename({ extname: '.min.js' }))
-      .pipe(gulp.dest('./build/js')) 
+gulp.task('js-build', function () {
+    gulp.src('js/*.js')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sourcemaps.init())
+        .pipe(babel())
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(gulp.dest('./build/js'))
 });
 
-gulp.task('lint', function() {
-    return gulp.src(['js/*.js','!node_modules/**'])
+gulp.task('lint', function () {
+    return gulp.src(['js/*.js', '!node_modules/**'])
         .pipe(plumber(plumberErrorHandler))
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 });
 
-gulp.task('BrowserReload', function(done) {
-      browserSync.reload();
-      done();
+gulp.task('BrowserReload', function (done) {
+    browserSync.reload();
+    done();
 })
 
-gulp.task('sass-build', function() {
-   gulp.src('sass/style.scss')
-      .pipe(plumber(plumberErrorHandler))
-      .pipe(sass())
-      .pipe(autoprefixer({
-         browsers: ['last 2 versions']
-      }))
-      .pipe(gulp.dest('build/css'))
-      .pipe(cssnano())
-      .pipe(rename('style.min.css'))
-      .pipe(gulp.dest('build/css'));
+gulp.task('sass-build', function () {
+    gulp.src('sass/style.scss')
+        .pipe(plumber(plumberErrorHandler))
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('build/css'))
+        .pipe(cssnano())
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('build/css'));
 });
 
 gulp.task('watch', function () {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: './'
         }
     });
 
-    gulp.watch("js/*.js", ['js-build', 'lint']);
-    gulp.watch("sass/*.scss", ['sass-build']);
-    
-    gulp.watch(["build/css/*", "build/js/*", "index.html"], ["BrowserReload"]);
+    gulp.watch('js/*.js', ['js-build', 'lint']);
+    gulp.watch('sass/*.scss', ['sass-build']);
+
+    gulp.watch(['build/css/*', 'build/js/*', 'index.html'], ['BrowserReload']);
 });
 
 gulp.task('default', ['watch', 'lint']);
--- js/main.js
+++ js/main.js
@@ -1,62 +1,63 @@
-$(function() {
-  
-var firstTimeSelecting = true;
- 
-  $('#section-select').on('change', function() {
-    if (firstTimeSelecting) {      
-      var el = $('header'),
-      curHeight = el.height(),
-      autoHeight = el.css('height', 'auto').height();
-      
+$(function () {
+
+  var firstTimeSelecting = true;
+  var el = $('header');
+  $('#section-select').on('change', function () {
+    if (firstTimeSelecting) {
+      var curHeight = el.height(),
+        autoHeight = el.css('height', 'auto').height();
+
       el.height(curHeight);
       el.height(autoHeight);
-      
+
       firstTimeSelecting = false;
+
     }
-    var $newsStoriesList = $('#news-stories-list');    
+    var $newsStoriesList = $('#news-stories-list');
     var selection = $('#section-select').val();
 
-    var url = 'https://api.nytimes.com/svc/topstories/v2/'+selection+'.json';
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + selection + '.json';
     url += '?' + $.param({
       'api-key': '193e3d7d6bf949bc9e5d62ae2565e5eb'
     });
-   $('.loading-image').show();
+    $('.loading-image').show();
     $.ajax({
       url: url,
       method: 'GET',
     })
-    .done(function(data) {        
+      .done(function (data) {
         var newsStoriesMarkup = '';
-        
-        var filteredData = data.results.filter(function(value){
+
+        var filteredData = data.results.filter(function (value) {
           return value.multimedia.length >= 5;
         });
-        
+
         filteredData.splice(12);
-        
-        $.each(filteredData, function ( key, value ) {
-            newsStoriesMarkup += '<li>';
-            newsStoriesMarkup += '<a href="'+value.url+'">'
-            newsStoriesMarkup += '<div class="article-background" style="background-image:url('+ value.multimedia[4].url + ')">';
-            newsStoriesMarkup += '<p class="article-abstract">'+value.abstract;
-            newsStoriesMarkup += '</p></div></a></li>';
+
+        $.each(filteredData, function (key, value) {
+          newsStoriesMarkup += '<li>';
+          newsStoriesMarkup += '<a href="' + value.url + '">'
+          newsStoriesMarkup += '<div class="article-background" style="background-image:url(' + value.multimedia[4].url + ')">';
+          newsStoriesMarkup += '<p class="article-abstract">' + value.abstract;
+          newsStoriesMarkup += '</p></div></a></li>';
         })
 
-      $newsStoriesList.html(newsStoriesMarkup);
-    })
-    .fail(function(err) {
-      throw err;
-    })
-    .always(function(){              
-      $('.loading-image').hide();
-    })    
+        $newsStoriesList.html(newsStoriesMarkup);
+        el.height('auto');
+      })
+      .fail(function (err) {
+        throw err;
+      })
+      .always(function () {
+        $('.loading-image').hide();
+      })
   });
-  
-  $('#news-stories-list').on('mouseenter', 'li', function(){
+
+  $('#news-stories-list').on('mouseenter', 'li', function () {
     $(this).find('.article-abstract').slideDown(1000);
   });
 
-  $('#news-stories-list').on('mouseleave', 'li', function(){
+  $('#news-stories-list').on('mouseleave', 'li', function () {
     $(this).find('.article-abstract').slideUp(1000);
   });
 });
